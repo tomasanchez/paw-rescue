b43082016b74b86e2906d3a9ac33766a7769b216
Tomas <tosanchez@frba.utn.edu.ar>
2021-05-03 23:15:00 -0300

    feat: caracteristicas optimas

---
+++ src/main/java/Caracteristica.java
@@ -0,0 +1,45 @@
+import java.util.Objects;
+
+/**
+ * Caracteristicas de una Mascota
+ * 
+ * 
+ * @since 05.03.2021
+ * @version 1.0
+ * @author Tomás Sánchez
+ */
+public class Caracteristica {
+
+  /**
+   * Los tipos de Caracteristicas disponibles
+   * 
+   * @since 05.03.2021
+   * @version 1.0
+   * @author Tomás Sánchez
+   */
+  static public enum TipoCaracteristica {
+    COLOR1, COLOR2, CASTRADO, MANCHAS, OTRO
+  }
+
+  /**
+   * La categoría de la caracteristica
+   */
+  TipoCaracteristica caracteristica;
+
+  /**
+   * Valor semántico de la característica, la característica propiamente dicha.
+   */
+  Object valor;
+
+  /**
+   * Instancia caracteristicas no nulas.
+   * 
+   * @param caracteristica La "categoria" de la caracteristica
+   * @param valor          la característica en sí
+   */
+  public Caracteristica(Caracteristica.TipoCaracteristica caracteristica, Object valor) {
+    this.caracteristica = Objects.requireNonNull(caracteristica);
+    this.valor = Objects.requireNonNull(valor);
+  }
+
+}
