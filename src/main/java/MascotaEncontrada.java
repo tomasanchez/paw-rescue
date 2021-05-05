bdbe46b931b24176fbb1757938bbbade0eb14969
Tomas <tosanchez@frba.utn.edu.ar>
2021-05-05 20:21:21 -0300

    feat: filtros de mascotas encontradas  por dia

--- src/main/java/MascotaEncontrada.java
+++ src/main/java/MascotaEncontrada.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.List;
 
 public class MascotaEncontrada {
@@ -5,11 +6,13 @@ public class MascotaEncontrada {
   List<String> foto;
   String descripcion;
   Coordenada lugar;
+  LocalDate fecha;
 
-  public MascotaEncontrada(List<String> foto, String descripcion, Coordenada lugar) {
+  public MascotaEncontrada(List<String> foto, String descripcion, Coordenada lugar, LocalDate fecha) {
     this.foto = foto;
     this.descripcion = descripcion;
     this.lugar = lugar;
+    this.fecha = fecha;
   }
 
 }
