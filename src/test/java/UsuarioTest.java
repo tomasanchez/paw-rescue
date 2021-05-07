66192c55a0b4019a4a90bd55f2c155e1c10f46ac
Tomas <tosanchez@frba.utn.edu.ar>
2021-05-07 11:10:53 -0300

    test: arreglo test de usuarios

--- src/test/java/UsuarioTest.java
+++ src/test/java/UsuarioTest.java
@@ -1,13 +1,10 @@
-/*
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
+
 
 public class UsuarioTest {
 
@@ -15,7 +12,7 @@ public class UsuarioTest {
   public void noUsuariosVacios() {
     // No crear usuarios con mascotas vacíos
     assertThrows(NullPointerException.class, () -> {
-      new RegistroUsuario().duenioMascota();
+      new RegistroUsuario().mascotaOwner();
     });
   }
 
@@ -28,37 +25,37 @@ public class UsuarioTest {
 
   @Test
   public void usuarioTieneUnaMascota() {
-    // Listado de una unica mascota
-    List<Mascota> mascotas = Arrays.asList(new Mascota[] { MascotaTest.perritaDePrueba("Maya") });
+
+    DuenioMascota owner = usuarioDePruebas("Usuario", "1 Mascota")
+        .mascota(MascotaTest.perritaDePrueba("Maya")).mascotaOwner();
     // Tiene exactamente una mascota
-    Assertions
-        .assertTrue(usuarioDePruebas("Usuario", "1 Mascota").mascotas(mascotas).duenioMascota().size() == 1);
+    Assertions.assertTrue(owner.getCantidadMascotas() == 1);
   }
 
   @Test
   public void usuarioTieneMultiplesMascota() {
-    // Listado de una unica mascota
-    List<Mascota> mascotas = Arrays.asList(new Mascota[] { MascotaTest.perritaDePrueba("Maya"),
-        MascotaTest.gatitoDePrueba("Tom"), MascotaTest.perritaDePrueba("Lola") });
-    // Tiene exactamente una mascota
-    Assertions.assertTrue(usuarioDePruebas("Usuario", "Muchas Mascotas").mascotas(mascotas).duenioMascota()
-        .size() == mascotas.size());
+    // Un owner de Mascotas con 3 mascotas. "Tom, Laya y Maya"
+    DuenioMascota owner = usuarioDePruebas("Usuario", "1 Mascota")
+        .mascota(MascotaTest.perritaDePrueba("Maya")).mascota(MascotaTest.perritaDePrueba("Laya"))
+        .mascota(MascotaTest.gatitoDePrueba("Tom")).mascotaOwner();
+    // Tiene exactamente 3 mascotasd
+    Assertions.assertTrue(owner.getCantidadMascotas() == 3);
   }
 
   /**
    * Instancia un usuario de pruebas
    * 
-   * @param nombre   Nombre del uisuario de prueba
+   * @param nombre Nombre del uisuario de prueba
    * @param apellido Apellido del usuario de Prueba
    * @return un Registro de usuario
-   
+   */
   static private RegistroUsuario usuarioDePruebas(String nombre, String apellido) {
     RegistroUsuario registro = new RegistroUsuario();
     // Registro de los datos personales
     registro.nombre(nombre).apellido(apellido).fechaNacimiento(LocalDate.now())
-      .contacto(contacoDePrueba(nombre, apellido)).tipoDocumento(Documento.TipoDocumento.DNI)
-      .numeroDocumento(00112233L);
-    // Registro de los datos de Usuario
+        .contacto(contacoDePrueba(nombre, apellido)).tipoDocumento(Documento.TipoDocumento.DNI)
+        .numeroDocumento(00112233L);
+    // Registro de los datos de Usuario return
     return registro.usuario(apellido + "-" + nombre).password("P@sWorD3prUb4+_!");
   }
 
@@ -66,10 +63,10 @@ public class UsuarioTest {
    * Instancia un dato de contacto
    * 
    * @return un dato de contacto
-   
+   */
   static private Contacto contacoDePrueba(String nombre, String apellido) {
     return new Contacto(nombre, apellido, "+5491100000000", "mail@test.com");
   }
 
 }
-*/
+
