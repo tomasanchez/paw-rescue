33f4dc3e06ca42ddae7dbe29aa8278c08e0da384
Tomas <tosanchez@frba.utn.edu.ar>
2021-05-31 19:01:24 -0300

    refactor(caracteristicas): elimino el enum #1

--- src/main/java/Caracteristica.java
+++ src/main/java/Caracteristica.java
@@ -1,44 +1,16 @@
-import java.util.Objects;
-
 /**
  * Caracteristicas de una Mascota.
  *
- *
  * @since 05.03.2021
- * @version 1.0
+ * @version 2.0
  * @author Tomás Sánchez
  */
 public class Caracteristica {
 
-  /**
-   * La categoría de la caracteristica.
-   */
-  TipoCaracteristica caracteristica;
+  String valor;
 
-  /**
-   * Valor semántico de la característica, la característica propiamente dicha.
-   */
-  Object valor;
-
-  /**
-   * Instancia caracteristicas no nulas.
-   *
-   * @param caracteristica La "categoria" de la caracteristica.
-   * @param valor          la característica en sí.
-   */
-  public Caracteristica(TipoCaracteristica caracteristica, Object valor) {
-    this.caracteristica = Objects.requireNonNull(caracteristica);
-    this.valor = Objects.requireNonNull(valor);
+  public Caracteristica(String valor) {
+    this.valor = valor;
   }
 
-  public TipoCaracteristica getCaracteristica() {
-    return this.caracteristica;
-  }
-
-  public Object getValor() {
-    return this.valor;
-  }
-}
-enum TipoCaracteristica {
-  COLOR1, COLOR2, CASTRADO, MANCHAS, OTRO
 }
