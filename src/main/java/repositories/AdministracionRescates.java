e7b39850303e08326c9ae1e0f97cfe8783fcfe10
Tomas <tosanchez@frba.utn.edu.ar>
2021-06-02 12:45:47 -0300

    refactor(repositorios): los muevo a una carpeta

---
+++ src/main/java/repositories/AdministracionRescates.java
@@ -0,0 +1,44 @@
+package repositories;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import model.usuario.Rescatista;
+
+public class AdministracionRescates {
+
+  List<Rescatista> rescates;
+
+  public AdministracionRescates() {
+    this.rescates = new ArrayList<>();
+  }
+
+  /**
+   * Obtiene las mascotas encontradas en los últimos días.
+   *
+   * @param dias ultimos días en los que filtrar
+   * @return las mascotas filtradas
+   */
+  List<Rescatista> mascotasEncontradas(long dias) {
+    return rescates.stream().filter(rescate -> rescate.compararFechaMascotaEncontrada(dias))
+        .collect(Collectors.toList());
+  }
+
+  /**
+   * Guarda la información de una mascota rescatada.
+   *
+   * @param rescatista el rescatista rescatada
+   */
+  public void registrarRescate(Rescatista rescatista) {
+    rescates.add(rescatista);
+  }
+
+
+  public List<Rescatista> getMascotasEncontradas() {
+    return rescates;
+  }
+
+
+}
+
+
---
+++ src/main/java/repositories/AdministracionUsers.java
@@ -0,0 +1,36 @@
+package repositories;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.mascota.encontrada.MascotaEncontrada;
+import model.usuario.DuenioMascota;
+
+public class AdministracionUsers {
+  private List<DuenioMascota> dueniosMascotas;
+
+  public AdministracionUsers() {
+    this.dueniosMascotas = new ArrayList<>();
+  }
+
+  public List<DuenioMascota> getDueniosMascotas() {
+    return this.dueniosMascotas;
+  }
+
+  public void registrarDuenioMascota(DuenioMascota duenioMascota) {
+    dueniosMascotas.add(duenioMascota);
+  }
+
+  // modificar
+  void notificarDuenioMascotaEncontrada(DuenioMascota duenioMascota) {}
+
+  DuenioMascota buscarDueño(MascotaEncontrada mascotaEncontrada) {
+    return (DuenioMascota) dueniosMascotas.stream()
+        .filter(duenio -> duenio.esDuenio(mascotaEncontrada));
+  }
+
+  // modificar
+  public void notificarDuenioMascotaPerdida(MascotaEncontrada mascotaEncontrada) {
+    notificarDuenioMascotaEncontrada(buscarDueño(mascotaEncontrada));
+  }
+
+}
