7c68adbdc83f22be85c7614c0bd5e6398cc5ed9e
Tomas <tosanchez@frba.utn.edu.ar>
2021-06-24 18:42:28 -0300

    refactor(repo):  requiere si o si mascotas

--- src/main/java/repositories/RepoRescates.java
+++ src/main/java/repositories/RepoRescates.java
@@ -2,6 +2,7 @@ package repositories;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import model.mascota.encontrada.MascotaEncontrada;
 import model.publicacion.Asociacion;
@@ -36,18 +37,20 @@ public class RepoRescates {
    */
   List<Rescatista> mascotasEncontradas(long dias) {
     return rescates.stream().filter(rescate -> rescate.estaDentroDeUltimosDias(dias))
-      .collect(Collectors.toList());
+        .collect(Collectors.toList());
   }
+
   /**
    * Guarda la información de una mascota rescatada.
    *
    * @param rescate el rescatista rescatada
    */
-  public void addRescate(Rescatista rescate){
+  public void addRescate(Rescatista rescate) {
+    Objects.requireNonNull(rescate.getMascotaEncontrada());
     rescates.add(rescate);
   }
-  
-  
+
+
   public Asociacion buscarAsociacion(MascotaEncontrada mascotaEncontrada) {
     ordenarAsociacionesPorDistancia(mascotaEncontrada);
     return asociaciones.get(1);
@@ -55,7 +58,7 @@ public class RepoRescates {
 
   private void ordenarAsociacionesPorDistancia(MascotaEncontrada mascotaEncontrada) {
     asociaciones.sort((asociacion1, asociacion2) -> asociacion1
-      .compararAsociacionesPorDistancia(asociacion2, mascotaEncontrada));
+        .compararAsociacionesPorDistancia(asociacion2, mascotaEncontrada));
   }
 
   /**
@@ -72,13 +75,14 @@ public class RepoRescates {
   private Rescatista buscarRescatista(MascotaEncontrada mascota) {
     return (Rescatista) rescates.stream().filter(rescatista -> rescatista.esSuMascota(mascota));
   }
-  
+
   public List<Refugio> buscarRefugios(MascotaEncontrada mascota) {
     List<Refugio> refugioList = proveedorRefugios.getAllRefugios();
     return refugioList.stream()
-      .filter(cada -> cada.getAdmisiones().contains(mascota.getTipoMascota()))
-      .collect(Collectors.toList());
+        .filter(cada -> cada.getAdmisiones().contains(mascota.getTipoMascota()))
+        .collect(Collectors.toList());
   }
+
   public List<Rescatista> getRescates() {
     return rescates;
   }
@@ -95,7 +99,7 @@ public class RepoRescates {
    */
   List<Rescatista> getRescatesEnLosUltimosDias(long dias) {
     return getRescates().stream().filter(rescate -> rescate.estaDentroDeUltimosDias(dias))
-      .collect(Collectors.toList());
+        .collect(Collectors.toList());
   }
 
   /**
@@ -104,13 +108,10 @@ public class RepoRescates {
    * @return un listado de mascotas.
    */
   public List<MascotaEncontrada> getMascotasEncontradas() {
-    return rescates.stream().map(Rescatista::getMascotaEncontrada)
-      .collect(Collectors.toList());
+    return rescates.stream().map(Rescatista::getMascotaEncontrada).collect(Collectors.toList());
   }
 
- 
 
-  
 
 }
 
