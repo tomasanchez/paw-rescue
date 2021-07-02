dc2bec3b9626614f69edd979ab4b0bb1c5ff2677
Tomas <tosanchez@frba.utn.edu.ar>
2021-07-02 17:29:52 -0300

    style(app): logs

--- src/main/java/app/RescatePatitas.java
+++ src/main/java/app/RescatePatitas.java
@@ -14,6 +14,7 @@ import services.mascota.RecomendadorDeAdopcion;
 public class RescatePatitas {
 
   public static void main(String[] args) {
+    System.out.println("Iniciando tareas programadas.");
     RepoPubParaAdoptar repoInteresados = new RepoPubParaAdoptar();
     RepoPubDarEnAdopcion repoPubDarEnAdopcion = new RepoPubDarEnAdopcion();
     RecomendadorDeAdopcion recomendador =
