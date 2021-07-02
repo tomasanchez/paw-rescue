6de4d188b690672e9f73155314f843b12dd3a9bf
Tomas <tosanchez@frba.utn.edu.ar>
2021-07-02 12:41:09 -0300

    test(preferencias): recomendaciones de patios

--- src/test/java/model/preferencia/PreferenciaTest.java
+++ src/test/java/model/preferencia/PreferenciaTest.java
@@ -2,11 +2,14 @@ package model.preferencia;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import model.mascota.Mascota;
 import model.mascota.TipoMascota;
+import model.pregunta.Respuesta;
 import model.publicacion.PublicacionDarEnAdopcion;
 
 public class PreferenciaTest {
@@ -57,6 +60,37 @@ public class PreferenciaTest {
     Assertions.assertTrue(preferencia.puedeRecomendarse(publicacion));
   }
 
+
+  @Test
+  void seRecomiendaSinPreferenciasDePatio() {
+    preferencia = new PreferenciaPatio(false);
+    when(publicacion.getRespuestas()).thenReturn(new ArrayList<>());
+    Assertions.assertTrue(preferencia.puedeRecomendarse(publicacion));
+  }
+
+
+  @Test
+  void seRecomiendaAlPreferirsePatioGrande() {
+    preferencia = new PreferenciaPatio(true);
+    crearRespuestasDePatio(true);
+    Assertions.assertTrue(preferencia.puedeRecomendarse(publicacion));
+  }
+
+  @Test
+  void noRecomiendaAlTenerPatioChicoYRequiereGrande() {
+    preferencia = new PreferenciaPatio(false);
+    crearRespuestasDePatio(true);
+    Assertions.assertFalse(preferencia.puedeRecomendarse(publicacion));
+  }
+
+  private void crearRespuestasDePatio(boolean patioGrande) {
+    List<Respuesta> rtas = new ArrayList<>();
+    Respuesta rta = mock(Respuesta.class);
+    rtas.add(rta);
+    when(publicacion.getRespuestas()).thenReturn(rtas);
+    when(rta.isSobre("patio")).thenReturn(true);
+    when(rta.isRespuesta("grande")).thenReturn(patioGrande);
+  }
 
   private void preferirGato() {
     preferencia = new PreferenciaDeMascota(TipoMascota.GATO);
