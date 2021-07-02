8a032b9fcf741eab3ece6d544cfa8511b5057ed9
Tomas <tosanchez@frba.utn.edu.ar>
2021-07-02 10:39:20 -0300

    refactor(extraction): validacion de respuestas

--- src/main/java/model/pregunta/Respuesta.java
+++ src/main/java/model/pregunta/Respuesta.java
@@ -1,19 +1,16 @@
 package model.pregunta;
 
+import java.util.Objects;
 import exceptions.RespuestaInvalida;
 
 
 public class Respuesta {
-  Pregunta pregunta;
-  String respuesta;
+  private Pregunta pregunta;
+  private String respuesta;
 
   public Respuesta(Pregunta pregunta, String respuesta) {
+    estaDentroDeLasRespuestas(pregunta, respuesta);
     this.pregunta = pregunta;
-    if (pregunta.getPosiblesRespuestas() != null &&
-      !pregunta.getPosiblesRespuestas().contains(respuesta)) {
-      throw new RespuestaInvalida(
-        "La respuesta no se encuentra entre las posibles respuestas de la pregunta");
-    }
     this.respuesta = respuesta;
   }
 
@@ -24,4 +21,20 @@ public class Respuesta {
   public String getRespuesta() {
     return respuesta;
   }
+
+  /**
+   * Valida si la respuesta es admitida
+   * 
+   * @param pregunta la pregunta
+   * @param respuesta la respuesta contestada
+   * @throws RespuestaInvalida si las posibles respues no son nulas, y la respuesta no esta incluida
+   */
+  private void estaDentroDeLasRespuestas(Pregunta pregunta, String respuesta) {
+    if (!Objects.isNull(pregunta.getPosiblesRespuestas())
+        && !pregunta.getPosiblesRespuestas().contains(respuesta)) {
+      throw new RespuestaInvalida(
+          "La respuesta no se encuentra entre las posibles respuestas de la pregunta");
+    }
+  }
+
 }
