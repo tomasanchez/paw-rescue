21386ad0ca0b452f187fdd9eb62092fdae1ed54a
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-09-28 17:39:51 -0300

    feat(voluntario): ORM

--- src/main/java/model/usuario/Voluntario.java
+++ src/main/java/model/usuario/Voluntario.java
@@ -1,10 +1,16 @@
 package model.usuario;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import model.publicacion.Asociacion;
 
+@Entity
 public class Voluntario extends Usuario {
 
+  @ManyToOne
+  @JoinColumn(name = "asociacion_id")
   Asociacion asociacion;
 
   public Asociacion getAsociacion() {
