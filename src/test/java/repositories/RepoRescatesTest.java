ac2c6b4b5a3a2dcb9c842c26d8291d887b455596
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-09-30 21:50:28 -0300

    test: updated para cumplir con el repo nuevo

--- src/test/java/repositories/RepoRescatesTest.java
+++ src/test/java/repositories/RepoRescatesTest.java
@@ -1,52 +1,61 @@
 package repositories;
 
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
-import java.time.LocalDate;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
+
 import model.mascota.encontrada.MascotaEncontrada;
 import model.usuario.Rescate;
 import model.usuario.datospersonales.DatosPersonales;
 import model.usuario.datospersonales.contacto.DatosContacto;
 import services.ServicioRescate;
 
-public class RepoRescatesTest {
+public class RepoRescatesTest implements WithGlobalEntityManager {
 
-  private RepoRescates repo= spy(RepoRescates.class);
-  private Rescate rescate= spy(new Rescate());
-  private ServicioRescate servicioRescate= spy(new ServicioRescate(null, repo, null));
-  private MascotaEncontrada mascota= mock(MascotaEncontrada.class);
-  
+  private RepoRescates repo;
+  private RepoPubRescate publicaciones;
+  private RepoUsers users;
+  private Rescate rescate;
+  private ServicioRescate servicioRescate;
+  private MascotaEncontrada mascota;
+
   @BeforeEach
   void initRepo() {
-    when(rescate.getMascotaEncontrada()).thenReturn(mascota);
-    doNothing().when(servicioRescate).identificarMascota(mascota);
+    entityManager().getTransaction().begin();
+    repo = new RepoRescates();
+    publicaciones = new RepoPubRescate();
+    users = new RepoUsers();
+    servicioRescate = spy(new ServicioRescate(publicaciones, repo, users));
+    mascota = new MascotaEncontrada();
+    rescate = new Rescate();
+    rescate.setMascotaEncontrada(mascota);
+  }
 
+  @AfterEach
+  void endTransaction() {
+    entityManager().getTransaction().rollback();
   }
 
   @Test
   void testAddRescate() {
     servicioRescate.registrarRescate(rescate);
-    Assertions.assertTrue(repo.getRescates().contains(rescate));
+    entityManager().flush();
+    Assertions.assertFalse(repo.getEntitySet().isEmpty());
   }
-
 
   @Test
   void testGetContactoRescatista() {
-    DatosPersonales datos = mock(DatosPersonales.class);
-    when(datos.getDatosContacto()).thenReturn(mockContacto());
-    doReturn(datos).when(rescate).getDatosRescatista();
+    DatosPersonales datos = new DatosPersonales();
+    datos.setContacto(mockContacto());
+    rescate.setDatosRescatista(datos);
     servicioRescate.registrarRescate(rescate);
-    Assertions.assertEquals(mockContacto().getMail(),
-        repo.contactoRescatista(mascota).getMail());
+    Assertions.assertEquals(mockContacto().getMail(), repo.contactoRescatista(mascota).getMail());
   }
 
-  
   @Test
   void testGetMascotasEncontradas() {
     servicioRescate.registrarRescate(rescate);
@@ -60,14 +69,12 @@ public class RepoRescatesTest {
 
   @Test
   void testGetRescatesEnLosUltimosDias() {
-    when(rescate.getMascotaEncontrada()).thenReturn(spy(new MascotaEncontrada()));
     repo.addRescate(rescate);
-    doReturn(LocalDate.now()).when(rescate).getFechaRescate();
     Assertions.assertTrue(repo.getRescatesEnLosUltimosDias(10).contains(rescate));
   }
-  
+
   DatosContacto mockContacto() {
     return new DatosContacto("Tomas", "Dias", "324432", "tomasDias@gmail.com");
   }
- 
+
 }
