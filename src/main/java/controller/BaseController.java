39ca302fb5dafecd9cb508a29e317dcb27cc9132
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-10-30 13:27:42 -0300

    feat(controller): superclass controller

---
+++ src/main/java/controller/BaseController.java
@@ -0,0 +1,79 @@
+package controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+
+public abstract class BaseController {
+
+  /**
+   * View Model.
+   */
+  private Map<String, Object> model = new HashMap<String, Object>();
+
+  /**
+   * Obtiene el nombre del controlador.
+   * 
+   * ? Ej: HomeController => Home
+   * 
+   * @return el nombre del controller
+   */
+  public String getControllerName() {
+    return getClass().getSimpleName().toLowerCase().replace("controller", "");
+  }
+
+  /**
+   * Obtiene el nombre del archivo de la vista.
+   * 
+   * ? Ej: HomeController => Home.hbs.html
+   * 
+   * @return el View Name
+   */
+  public String getViewName() {
+    return this.getControllerName().concat(".html.hbs");
+  }
+
+  /**
+   * Obtiene el HTTP path.
+   * 
+   * ? Ej: HomeController => /Home
+   * 
+   * @return
+   */
+  public String getPath() {
+    return "/".concat(this.getClass().getSimpleName().toLowerCase().replace("controller", ""));
+  }
+
+  public Map<String, Object> getModel() {
+    return model;
+  }
+
+  public void setModel(Map<String, Object> model) {
+    this.model = model;
+  }
+
+
+  /**
+   * Obtiene la ruta del viewmodel.
+   * 
+   * @param request la HTTP request
+   * @param response la HTTP response
+   * @return el ViewModel
+   */
+  public ModelAndView getViewModel(Request request, Response response) {
+    this.onInit(request, response);
+    return new ModelAndView(this.getModel(), this.getViewName());
+  }
+
+  /**
+   * Funcion llamada antes de obtener el View Model.
+   * 
+   * @param request la HTTP request.
+   * @param response la HTTP response.
+   */
+  protected abstract void onInit(Request request, Response response);
+
+
+}
---
+++ src/main/java/services/controller/ControllerService.java
@@ -0,0 +1,60 @@
+package services.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import controller.BaseController;
+import controller.LoginController;
+
+public class ControllerService {
+
+
+  private static ControllerService instance;
+  private Map<String, BaseController> controllers;
+
+  private ControllerService() {
+    controllers = new HashMap<String, BaseController>();
+  }
+
+  public static ControllerService getInstance() {
+    if (Objects.isNull(instance)) {
+      instance = new ControllerService();
+      initControllers();
+    }
+    return instance;
+  }
+
+  public Map<String, BaseController> getControllers() {
+    return controllers;
+  }
+
+  public List<BaseController> getControllersList() {
+    return controllers.values().stream().collect(Collectors.toList());
+  }
+
+  /**
+   * Añade un controller a la lista.
+   * 
+   * @param controller un nuevo controller a agregar
+   */
+  public static void addController(BaseController controller) {
+    getInstance().getControllers().put(controller.getControllerName(), controller);
+  }
+
+  public BaseController getController(String controllerName) {
+    return getInstance().getControllers().get(controllerName);
+  }
+
+
+  /**
+   * Inicializar todos los controllers.
+   */
+  private static void initControllers() {
+    // ! Todos los controladores deben estar aquí
+    // TODO: Agregar controllers
+    addController(new LoginController());
+  }
+
+}
