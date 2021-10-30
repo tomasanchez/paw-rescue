ecdb5dc5c506b3ed5253f202d32cc89ed96cffb0
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-10-30 13:27:55 -0300

    feat(controller): login

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import spark.Request;
+import spark.Response;
+
+public class LoginController extends BaseController {
+
+  @Override
+  protected void onInit(Request request, Response response) {
+    // TODO Auto-generated method stub
+  }
+
+}
