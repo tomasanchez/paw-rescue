69828c15111b6c90eb7885c4db17da3ebbdae5b0
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-11-03 01:00:50 -0300

    feat(controller): login post

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,13 +1,39 @@
 package controller;
 
+import java.util.Objects;
+import app.Router;
+import model.usuario.Usuario;
+import repositories.RepoUsers;
+import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
+import spark.Spark;
 
 public class LoginController extends BaseController {
 
   @Override
   protected void onInit(Request request, Response response) {
-    // TODO Auto-generated method stub
+    Spark.post(this.getPath(), (req, res) -> this.onLogin(req, res), Router.getEngine());
   }
+
+  private ModelAndView onLogin(Request request, Response response) {
+
+    String uname = request.queryParams("user");
+    String password = request.queryParams("password");
+
+    if (Objects.isNull(uname) || Objects.isNull(password)) {
+      response.status(400);
+      return null;
+    }
+
+    Usuario user = RepoUsers.getInstance().getLogin(uname, password);
+
+    this.getModel().put("username", uname);
+    this.getModel().put("isValid", (Objects.isNull(user) ? "is-invalid" : "is-valid"));
+
+    return this.getViewModel();
+  }
+
+
 
 }
