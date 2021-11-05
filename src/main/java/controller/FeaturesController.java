4737fb545f66f549398fd6b6519c18f6a2aacc03
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-11-05 20:52:00 -0300

    feat(controller): update and delete

--- src/main/java/controller/FeaturesController.java
+++ src/main/java/controller/FeaturesController.java
@@ -1,13 +1,71 @@
 package controller;
 
+import app.Router;
+import model.mascota.caracteristica.Caracteristica;
+import repositories.RepoCaracteristicas;
+import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
+import spark.Spark;
 
 public class FeaturesController extends BaseController {
 
   @Override
-  protected void onInit(Request request, Response response) {
+  protected void onInit() {
+    onSetPost();
+  }
+
+  @Override
+  protected void onBeforeRendering(Request request, Response response) {
     requiereSession(request, response);
+    updateFeaturesSet();
+  }
+
+  private void updateFeaturesSet() {
+    this.getModel().put("caracteristicas", RepoCaracteristicas.getInstance().getEntitySet());
+  }
+
+  private void onSetPost() {
+    Spark.post(this.getPath().concat("/:id"), (req, res) -> this.onPost(req, res),
+        Router.getEngine());
+  }
+
+  private ModelAndView onPost(Request req, Response res) {
+
+    Long id = Long.parseLong(req.params(":id"));
+    Boolean del = Boolean.parseBoolean(req.queryParams("delete"));
+
+    if (del) {
+      this.onDeleteFeature(id, res);
+    } else {
+      this.onUpdateFeature(id, req.queryParams("valor"), res);
+    }
+
+    res.redirect(this.getPath());
+
+    return null;
+  }
+
+  private void onUpdateFeature(Long id, String valor, Response res) {
+
+    try {
+      Caracteristica c = RepoCaracteristicas.getInstance().getEntity(id);
+      c.setValor(valor);
+      withTransaction(() -> RepoCaracteristicas.getInstance().updateEntity(c));
+      res.status(200);
+    } catch (RuntimeException e) {
+      res.status(500);
+    }
+
+  }
+
+  private void onDeleteFeature(Long id, Response res) {
+    try {
+      withTransaction(() -> RepoCaracteristicas.getInstance().deleteEntity(id));
+      res.status(200);
+    } catch (RuntimeException e) {
+      res.status(500);
+    }
   }
 
 }
