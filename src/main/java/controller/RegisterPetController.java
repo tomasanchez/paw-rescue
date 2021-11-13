ac8f8b6152d83ac5021ee61db2078d6bdcdda240
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-11-13 14:16:27 -0300

    refactor: using repo

--- src/main/java/controller/RegisterPetController.java
+++ src/main/java/controller/RegisterPetController.java
@@ -1,7 +1,9 @@
 package controller;
 
+import static spark.Spark.post;
+import java.util.Arrays;
+import java.util.List;
 import app.Router;
-import model.mascota.Chapita;
 import model.mascota.Mascota;
 import model.mascota.Sexo;
 import model.mascota.TipoMascota;
@@ -13,11 +15,6 @@ import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static spark.Spark.post;
-import static java.lang.Long.parseLong;
 
 public class RegisterPetController extends BaseController {
 
@@ -55,6 +52,20 @@ public class RegisterPetController extends BaseController {
   private ModelAndView onPost(Request req, Response res) {
     requiereSession(req, res);
     Usuario user = this.getLoggedUser(req);
+
+    try {
+      withTransaction(() -> user.registrarMascota(buildMascota(req)));
+      res.status(201);
+      res.redirect(ControllerService.getInstance().getController("pets").getPath());
+    } catch (RuntimeException e) {
+      res.status(500);
+      this.getModel().put("toastStatus", "bg-error");
+      this.getModel().put("toastMessage", getResourceBundle().getText("featureError"));
+    }
+    return this.getViewModel(req, res);
+  }
+
+  private Mascota buildMascota(Request req) {
     String nombre = req.queryParams("name");
     String apodo = req.queryParams("alias");
     int edad = Integer.parseInt(req.queryParams("age"));
@@ -63,53 +74,11 @@ public class RegisterPetController extends BaseController {
     List<String> fotos = Arrays.asList(req.queryParamsValues("photos"));
     String descripcionFisica = req.queryParams("description");
     List<String> caracteristicasTxt = Arrays.asList(req.queryParamsValues("characteristics"));
-    
-    Chapita chapita = new Chapita(user);
-    
-    try {
-      withTransaction(() ->
-        user.registrarMascota(
-          new Mascota(nombre, apodo, tipoMascota, edad, sexo, descripcionFisica, fotos, chapita)
-        )
-      );
-      
-      res.status(201);
-    }
-    catch (RuntimeException e) {
-      entityManager().remove(buscarMascota(chapita.getId()));
-      res.status(500);
-      this.getModel().put("toastStatus", "bg-error");
-      this.getModel().put("toastMessage", getResourceBundle().getText("featureError"));
-    }
-    setearCaracteristicas(chapita,caracteristicasTxt);
-    return this.getViewModel(req, res);
-  }
-
-  private void setearCaracteristicas(Chapita chapita, List<String> caracteristicasTxt){
-    long id = chapita.getId();
-    Mascota pet = buscarMascota(id);
-    for(String charId : caracteristicasTxt){
-    Caracteristica caracteristica = buscarCaracteristica(parseLong(charId));
-    pet.addCaracteristica(caracteristica);
-    }
-  }
-  
-  private Caracteristica buscarCaracteristica(long charID) {
-      Caracteristica caracteristica = (Caracteristica) entityManager()
-        .createQuery("FROM " + "Caracteristica"
-          + " C WHERE C.id LIKE :id ")
-        .setParameter("id", charID).getSingleResult();
-      return caracteristica;
-  }
-
-  private Mascota buscarMascota(long chapitaId) {
-    Mascota mascota = (Mascota) entityManager()
-      .createQuery("FROM " + "Mascota"
-        + " M WHERE M.chapita LIKE :id ")
-      .setParameter("id", chapitaId).getSingleResult();
+    List<Caracteristica> caracteristicas =
+        RepoCaracteristicas.getInstance().getEntitySet(caracteristicasTxt);
+    Mascota mascota =
+        new Mascota(nombre, apodo, tipoMascota, edad, sexo, descripcionFisica, fotos, null);
+    mascota.getCaracteristicas().addAll(caracteristicas);
     return mascota;
   }
-
-
-  
 }
--- src/main/java/repositories/RepoCaracteristicas.java
+++ src/main/java/repositories/RepoCaracteristicas.java
@@ -1,6 +1,10 @@
 package repositories;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import javax.persistence.NoResultException;
 import db.PersistentEntitySet;
 import model.mascota.caracteristica.Caracteristica;
 
@@ -9,13 +13,21 @@ public class RepoCaracteristicas extends PersistentEntitySet<Caracteristica> {
   private static RepoCaracteristicas instance;
 
   public static RepoCaracteristicas getInstance() {
-
     if (Objects.isNull(instance)) {
       instance = new RepoCaracteristicas();
     }
-
     return instance;
   }
 
+  @SuppressWarnings("unchecked")
+  public List<Caracteristica> getEntitySet(List<String> ids) {
+    List<Long> lids = ids.stream().map(id -> Long.valueOf(id)).collect(Collectors.toList());
+    try {
+      return entityManager().createQuery("FROM " + getTableName() + " C WHERE C.id IN :ids")
+          .setParameter("ids", lids).getResultList();
+    } catch (NoResultException e) {
+      return new ArrayList<>();
+    }
+  }
 
 }
