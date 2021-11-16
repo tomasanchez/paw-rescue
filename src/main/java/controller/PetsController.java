5008a583acca6076a1f4ea3f99b2a043fb100cbd
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-11-16 10:35:20 -0300

    feat(mvc): delete pet

--- src/main/java/controller/PetsController.java
+++ src/main/java/controller/PetsController.java
@@ -1,5 +1,10 @@
 package controller;
 
+import static spark.Spark.post;
+
+import app.Router;
+import repositories.RepoMascotas;
+import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
@@ -8,6 +13,7 @@ public class PetsController extends BaseController {
   @Override
   protected void onInit() {
     getModel().put("showToast", false);
+    onSetPost();
   }
 
   @Override
@@ -18,7 +24,43 @@ public class PetsController extends BaseController {
 
   @Override
   protected void onAfterRendering(Request request, Response response) {
-    // TODO Auto-generated method stub
+    onInitToast();
+  }
+
+  private void onSetPost() {
+    post(getPath().concat("/:id"), (req, res) -> this.onMerge(req, res), Router.getEngine());
+  }
+
+  private ModelAndView onMerge(Request req, Response res) {
+    requiereSession(req, res);
+
+    Long id = Long.parseLong(req.params(":id"));
+    Boolean del = Boolean.parseBoolean(req.queryParams("delete"));
+
+
+    try {
+      if (del) {
+        onDelete(id);
+      } else {
+        onUpdate(id, req);
+      }
+      onSwitchToast(true);
+      res.status(200);
+    } catch (RuntimeException e) {
+      onSwitchToast(false);
+      res.status(500);
+    }
+
+    return this.getViewModel();
+  }
+
+
+  private void onDelete(Long id) {
+    withTransaction(() -> new RepoMascotas().deleteEntity(id));
+  }
+
+  private void onUpdate(Long id, Request req) {
+
   }
 
 }
---
+++ src/main/java/repositories/RepoMascotas.java
@@ -0,0 +1,8 @@
+package repositories;
+
+import db.PersistentEntitySet;
+import model.mascota.Mascota;
+
+public class RepoMascotas extends PersistentEntitySet<Mascota> {
+
+}
